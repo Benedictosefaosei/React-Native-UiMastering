b693f35d0bde84cb75037020a9ae49602f1d64c2
benedictosefaosei <benedictosefaosei@gmail.com>
2025-12-06 17:50:39 -0500

    built my splash screen

--- src/screens/IntroScreen.tsx
+++ src/screens/IntroScreen.tsx
@@ -1,15 +1,29 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import FoodLogo from "../assets/FoodLogo";
+import SunImage from "../assets/SunImage";
 
 const IntroScreen = () => {
   return (
-    <View>
-      <FoodLogo style={{ marginTop: 50 }} />
+    <View style={styles.container}>
+      <FoodLogo />
+      <SunImage style={styles.sunImage} />
     </View>
   );
 };
 
 export default IntroScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  sunImage: {
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+  },
+});
